cc05e5798b8ff45cfe511b4d61ee284955c407ff
1Conan <me@1conan.com>
2020-11-28 14:55:41 +0800

    🎨 restructure client code

--- client/index.js
+++ client/index.js
@@ -3,47 +3,47 @@ const WebSocket = require('ws');
 const { SSHConnection } = require('node-ssh-forward');
 const Clipboard = require('./clipboard');
 
-const sshConnection = new SSHConnection({
-  endHost: process.env.DEVICE_IP,
-  password: process.env.SSH_PASSWORD,
-  privateKey: process.env.SSH_PRIVATEKEY ? fs.readFileSync(process.env.SSH_PRIVATEKEY) : undefined,
-  passphrase: process.env.SSH_PRIVATEKEY_PASSPHRASE,
-})
+let ws = undefined;
+let currentClip;
 
 // clipboard
 const clipboard = new Clipboard();
 
-let ws = undefined;
-let currentClip;
-
 clipboard.on('ready', async () => {
-  await sshConnection.forward({
-    fromPort: 6969,
-    toPort: 6969,
-  });
-
-  ws = new WebSocket(`ws://localhost:${6969}`);
-
-  ws.on('open', () => {
-    console.log('connected');
-  });
-  ws.on('close', () => process.exit());
-
-  ws.on('message', (data) => {
-    if (currentClip === data) return;
-    console.log('received:', data);
-    clipboard.write(data);
-    currentClip = data;
-  });
-
   console.log('clipboard started');
 })
 
 clipboard.on('change', (data) => {
   if (currentClip === data) return;
-  console.log('clipboard changed!', data);
+  console.log('clipboard changed!', data.trim());
   currentClip = data;
-  if (ws) ws.send(data);
+  if (ws && ws.readyState === 1) ws.send(data);
 })
 
-clipboard.start();
+const sshConnection = new SSHConnection({
+  endHost: process.env.DEVICE_IP,
+  password: process.env.SSH_PASSWORD,
+  privateKey: process.env.SSH_PRIVATEKEY ? fs.readFileSync(process.env.SSH_PRIVATEKEY) : undefined,
+  passphrase: process.env.SSH_PRIVATEKEY_PASSPHRASE,
+  skipAutoPrivateKey: !!process.env.SSH_SKIP_PRIVATEKEY,
+})
+
+async function main() {
+  await sshConnection.forward({
+    fromPort: 6969,
+    toPort: 6969,
+  });
+  ws = new WebSocket(`ws://localhost:${6969}`);
+
+  ws.on('open', () => console.log(`connected to ${process.env.DEVICE_IP}`));
+  ws.on('close', () => process.exit());
+  ws.on('message', (data) => {
+    if (currentClip === data) return;
+    console.log('received:', data.trim());
+    clipboard.write(data);
+    currentClip = data;
+  });
+
+  clipboard.start();
+}
+main();
