23320a1be7fe3f9d33d7935dec2e2eccc392ae74
1Conan <me@1conan.com>
2020-11-30 03:06:30 +0800

    🚸 pause before exit on windows

---
+++ CumSync/start.bat
@@ -0,0 +1,15 @@
+@echo off
+:: Your device's IP address
+SET DEVICE_IP=10.0.13.17
+:: this is fine
+SET SSH_USERNAME=mobile
+:: Change this ffs
+SET SSH_PASSWORD=alpine
+:: uncomment this if you wanna use ssh keys
+:: SET SSH_PRIVATEKEY=C:\sshkeys\id_bla
+:: uncomment this if your ssh key has a passphrase
+:: SET SSH_PRIVATEKEY_PASSPHRASE=imarart
+
+cumsync.exe
+
+pause
